e0d315cc287fb18ef8d8ca9ac05916b736e80fdb
Blake Swaidner <blake.swaidner@gmail.com>
2017-07-23 00:30:05 -0400

    DB-IOS-002 Integrated Firebase into app for Realtime Database on Events page; structured data for events; added childAdded, childRemoved, and childChanged observers to make data 100% realtime

--- app-danceblue/app-danceblue/Event.swift
+++ app-danceblue/app-danceblue/Event.swift
@@ -10,11 +10,26 @@ import Foundation
 
 class Event {
     
-    public dynamic var month = ""
-    public dynamic var date = ""
-    public dynamic var title = ""
-    public dynamic var location = ""
-    public dynamic var time = ""
+    public var month: String?
+    public var date: Int?
+    public var title: String?
+    public var location: String?
+    public var time: String?
+    public var id: String?
+    public var timestamp: Date?
     
+    init(from dictionary: [String : AnyObject]) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        
+        month = dictionary["month"] as? String
+        date = dictionary["date"] as? Int
+        title = dictionary["title"] as? String
+        location = dictionary["location"] as? String
+        time = dictionary["time"] as? String
+        id = dictionary["id"] as? String
+        timestamp = dateFormatter.date(from: dictionary["timestamp"] as! String)
+    }
     
 }
--- app-danceblue/app-danceblue/EventTableViewCell.swift
+++ app-danceblue/app-danceblue/EventTableViewCell.swift
@@ -17,16 +17,35 @@ class EventTableViewCell: UITableViewCell {
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var eventTitleLabel: UILabel!
-    @IBOutlet weak var eventLocation: UILabel!
-    @IBOutlet weak var eventTime: UILabel!
+    @IBOutlet weak var eventLocationLabel: UILabel!
+    @IBOutlet weak var eventTimeLabel: UILabel!
 
+    private var event: Event?
+    
+    // MARK: - Styling
+    
+    private var lightBackgroundColor: UIColor = Styles.eventsBackgroundColorLight
+    private var darkBackgroundColor: UIColor = Styles.eventsBackgroundColorDark
+    
+    // MARK: - Initialization
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        calendarBanner.layer.cornerRadius = 5.0
+        calendarBackground.layer.cornerRadius = 5.0
         
+        calendarBanner.clipsToBounds = true
+        calendarBackground.clipsToBounds = true
     }
 
-    func configureCell() {
+    func configureCell(with event: Event, for indexPath: IndexPath) {
+        monthLabel.text = event.month
+        dateLabel.text = "\(event.date ?? 0)"
+        eventTitleLabel.text = event.title
+        eventLocationLabel.text = event.location
+        eventTimeLabel.text = event.time
+        
+        self.backgroundColor = indexPath.row % 2 == 0 ? lightBackgroundColor : darkBackgroundColor
         
     }
     
